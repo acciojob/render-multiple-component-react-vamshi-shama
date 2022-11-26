05f86caafb7b3ccf55ac637731b827bf8bf9683d
vamshi <vamshis2017@gmail.com>
2022-11-26 23:22:20 +0530

    Added new lines

--- src/components/description.js
+++ src/components/description.js
@@ -2,7 +2,8 @@
 const Description = (props) => {
     return (
         <div className="Description">
-            
+            {props.firstPoint}
+            {props.secondPoint}
         </div>
     )
 }
